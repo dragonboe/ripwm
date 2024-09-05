b6c2a77849d183332bd74e179f44f896b69e2e17
Marla Wazker <urbuina@gmail.com>
2024-09-05 12:11:35 +0200

    Update wm_dll.c

--- wm_dll.c
+++ wm_dll.c
@@ -1,5 +1,5 @@
 // This DLL is loaded into many processes on the computer, therefore we need to keep the logic here as simple as possible to avoid slowing down the system
-#define LIGHTWM_DLL
+#define RIPWM_DLL
 
 #include <Windows.h>
 #include <stdio.h>
